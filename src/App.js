716efa885a61d03ceb921b6c18fda3591c106bde
Brendan Stone <stone@Stones-MacBook-Pro.local>
2022-12-25 11:45:37 -0500

    Final Updates

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ function App() {
     <div className='container'>
       <img className="comingSoon" src={comingSoonImg} alt="Coming Soon"/>
       <CountdownTimer/>
-      <div>
-        <span className='timerLabel'>DAYS</span>
-        <span className='timerLabel'>HOURS</span>
-        <span className='timerLabel'>MINUTES</span>
-        <span className='timerLabel'>SECONDS</span>
-      </div>
     </div>
   );
 }
--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -3,15 +3,33 @@ import Countdown from 'react-countdown';
 // Renderer callback with condition
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
     return (
-    <div className="timer">
-        <span>{days} : {hours} : {minutes} : {seconds}</span>
-    </div>
+    <table>
+        <tbody>
+            <tr>
+                <td className="timer">{days}</td>
+                <td className="timer">:</td>
+                <td className="timer">{hours}</td>
+                <td className="timer">:</td>
+                <td className="timer">{minutes}</td>
+                <td className="timer">:</td>
+                <td className="timer">{seconds}</td>
+            </tr>
+            <tr>
+                <td className='timerLabel'>DAYS</td>
+                <td></td>
+                <td className='timerLabel'>HOURS</td>
+                <td></td>
+                <td className='timerLabel'>MINUTES</td>
+                <td></td>
+                <td className='timerLabel'>SECONDS</td>
+            </tr>
+        </tbody>
+    </table>
     );
 };
 
 function CountdownTimer() {
     return <Countdown 
-        className="timer" 
         date={new Date('2023-06-01T00:00:00')}
         renderer={renderer}
     />;
